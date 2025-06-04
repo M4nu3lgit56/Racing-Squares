ab2de347652c781d75d7a066a8a5a121e42bc800
M4nu3lgit56 <manuelhmhd@gmail.com>
2025-06-04 13:07:14 -0500

    archivo js creado, html y CSS actualizado

---
+++ app.js
@@ -0,0 +1,7 @@
+// inicializando variables
+const container = document.getElementById('arena');
+const square = document.getElementById('sqr');
+const rect = container.getBoundingClientRect();
+const sqrRect = square.getBoundingClientRect();
+
+// 
